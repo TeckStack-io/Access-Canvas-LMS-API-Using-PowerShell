d0d925c852a3143855309566743db015e5ad0b48
Jeff Nelson <jenelson@hsvs.com>
2020-06-10 23:52:51 -0500

    update course id param

--- UpdateGradingStandard.ps1
+++ UpdateGradingStandard.ps1
@@ -2,21 +2,21 @@
 $accessToken = ""
 $headers = @{"Authorization"="Bearer "+$accessToken} # build access token header
 
-$canvasCourseUrl = 2102022
+$canvasCourseId = 2102022
 
 # get course data
-$canvasCourseUrl =  "$canvasDomain/courses/$canvasCourseUrl"
+$canvasCourseUrl =  "$canvasDomain/courses/$canvasCourseId"
 $canvasCourseResult = Invoke-RestMethod -Method GET -uri $canvasCourseUrl -header $headers  
 
 Write-Host $canvasCourseResult
 
 # update course grading scheme
 $updateCourseJson = @{"course[grading_standard_id]"=129230;}    
-$updateCourseUrl = "$canvasDomain/courses/$canvasCourseUrl"
+$updateCourseUrl = "$canvasDomain/courses/$canvasCourseId"
 $updateCourseResults = Invoke-RestMethod -Method put -uri $updateCourseUrl -header $headers -body $updateCourseJson  -contentType "application/x-www-form-urlencoded"
 
 # get course data
-$canvasCourseUrl =  "$canvasDomain/courses/$canvasCourseUrl"
+$canvasCourseUrl =  "$canvasDomain/courses/$canvasCourseId"
 $canvasCourseResult = Invoke-RestMethod -Method GET -uri $canvasCourseUrl -header $headers  
 
 Write-Host $canvasCourseResult
